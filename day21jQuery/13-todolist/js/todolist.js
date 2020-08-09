20592b2c1866ba5221e0264e375062e69fad8f7d
dawuhuang <jk996zz@163.com>
2020-08-09 21:57:40 +0800

    第21天代码-完成todolist所有功能

--- day21jQuery/13-todolist/js/todolist.js
+++ day21jQuery/13-todolist/js/todolist.js
@@ -13,7 +13,9 @@ $(function () {
         if (e.keyCode == 13) {
             // 先读取本地数据
             var local = getData();
-            console.log(local);
+            if ($(this).val().trim() == '') {
+                return
+            }
             local.push({ title: $(this).val(), done: false });
             saveData(local);
             $(this).val("");
@@ -22,7 +24,7 @@ $(function () {
     });
 
     //2 删除操作
-    $('#todolist').on('click','a',function() {
+    $('ul,ol').on('click','a',function() {
         var index = $(this).parent().attr('data-index')
         // 根据索引删除数据
         var arr = getData()
@@ -33,12 +35,13 @@ $(function () {
     })
 
     //3 点击复选框修改本地存储数据
-    $('section').on('change','input', function() {
-        index = $(this).parent().attr('data-index')
+    $('ol,ul').on('click','input', function() {
         var delDate = getData();
+        index = $(this).parent().attr('data-index')
+        // console.log(delDate[index]);
         delDate[index].done = $(this).prop('checked')
-        
-        // console.log(delDate[index].done);
+        saveData(delDate)
+        load()
     })
 
     // 读取本地存储里面的数据
@@ -57,20 +60,33 @@ $(function () {
 
     // 渲染到本地
     function load() {
-        // 先清空
-        $("#todolist").empty()
+
         // 先读取本地
         var loadDate = getData();
+        // 先清空 ol
+        // $("#todolist").empty()
+        // 8/8没有完成的原因是渲染的时候忘记清空本地存储里面的数据了。当时应该是脑袋糊掉了
+        // $("#donelist").empty()
+        $('ul,ol').empty()
+        // 代办个数
+        var todoCount = 0
+        // 已完成个数
+        var doneCount = 0
         $.each(loadDate, function (i, docEle) {
             // console.log($(docEle)[0].title);  
             // 首先 i是在each里面的，不会影响
             // console.log(docEle.done);
             if (docEle.done) {
-                $("#donelist").prepend(`<li data-index=${i}><input type="checkbox" ><p>${$(docEle)[0].title}</p><a href="javascript:;"></a></li>`);  
+                $("#donelist").prepend(`<li data-index=${i}><input type="checkbox" checked ><p><s>${docEle.title}</s></p><a href="javascript:;"></a></li>`);
+                doneCount++
             }else {
-                $("#todolist").prepend(`<li data-index=${i}><input type="checkbox"><p>${$(docEle)[0].title}</p><a href="javascript:;"></a></li>`);  
+                $("#todolist").prepend(`<li data-index=${i}><input type="checkbox"><p>${docEle.title}</p><a href="javascript:;"></a></li>`);
+                todoCount++
             }
-           
         });
+        // 结束后读取个数
+        $('#todocount').html(todoCount)
+        $('#donecount').html(doneCount)
     }
+
 });
