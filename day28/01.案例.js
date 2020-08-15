6e4792ebc6c018d6b9b524c4cd10c08c6bf93ccf
dawuhuang <jk996zz@163.com>
2020-08-15 18:02:30 +0800

    第28天案例优化

--- day28/01.案例.js
+++ day28/01.案例.js
@@ -1,50 +1,13 @@
 // 连接数据库
 const http = require('http')
-const mongoose = require('mongoose')
+
 const url = require('url')
 const querystring = require('querystring')
 
-
+require('./index')
+const User = require('./user')
 const app = http.createServer()
-mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true,useUnifiedTopology: true })
-.then( () => console.log('数据库连接成功'))
-.catch( err => console.log('数据库连接失败',err))
 
-
-// 创建集合规则
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: [2, '名字最小长度是2'],
-        maxlength: [6,'名字最大长度是6'],
-        required: [true, 'name是必填项'],
-        trim: true
-    },
-    age: {
-        type: Number,
-        min: [16,'最小年龄为16'],
-        max: [100,'最小年龄为100'],
-        required: [true, 'age是必填项'],
-        trim: true
-    },
-    email: {
-        type: String,
-        required: [true, 'email是必填项'],
-        trim: true
-    },
-    password: {
-        type: Number,
-        minlength: [6,'密码最小长度为6'],
-        maxlength: [16,'密码最大长度为16'],
-        required: [true,'密码是必填项']
-    },
-    hobbies: {
-        type: [String],
-        enum: ['足球','篮球','橄榄球','敲代码','抽烟','喝酒','烫头'],
-        required: [true, '爱好是必填项']
-    }
-})
-const User = mongoose.model('User',userSchema);
 
 app.on('request',async (req,res) => {
 
