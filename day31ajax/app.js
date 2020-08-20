c9d12397d6f1971f92f7fb1b85b6d079ffc7c8d6
dawuhuang <jk996zz@163.com>
2020-08-20 11:53:08 +0800

    第31天代码ajax请求类型

--- day31ajax/app.js
+++ day31ajax/app.js
@@ -2,9 +2,12 @@
 const express =require('express')
 // 引入路径处理模块
 const path = require('path')
+// 引入body-parser模块
+const bodyParser = require('body-parser')
 // 开启服务器
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
 
 // 开启静态资源访问功能
 app.use(express.static(path.join(__dirname,'public')))
@@ -14,7 +17,13 @@ app.get('/first',(req,res) =>{
     // 接受get方式发送过来的请求
     res.send('hello ajax')
 })
+app.get('/get',(req,res) => {
+    res.send(req.query)
+})
 
+app.post('/post',(req,res) => {
+    res.send(req.body)
+})
 // 监听端口
 app.listen(3000)
 // 告诉开发者服务器启动成功
