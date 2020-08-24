4c80ffb06dcbec49b7c699109fcf9bfb8ef3d6c7
dawuhuang <jk996zz@163.com>
2020-08-24 11:18:39 +0800

    第34天代码服务器端跨域

--- day34ajax/s1/app.js
+++ day34ajax/s1/app.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const request = require('request')
 
 const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname,'public')))
+app.get('/server',(req,res) => {
+    // err 错误对象  response 响应信息 body响应的主体内容
+    request('http://localhost:3001/see',(err,response,body) => {
+    // console.log(err);
+    // console.log(response);
+    // console.log(body);
+    res.send(body)
+    })
+})
 
 app.listen(3000)
 console.log('s1服务器启动成功');
--- day34ajax/s2/app.js
+++ day34ajax/s2/app.js
@@ -1,16 +1,24 @@
 const express = require('express')
-
+const formidable = require('formidable')
 const path = require('path')
-const { json } = require('express')
+var session = require('express-session')
 const app = express()
+
+// 实现session功能
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }));
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use((req,res,next) => {
 // 允许那些客户端访问我 *表示允许所以网站访问
-res.header('Access-Control-Allow-Origin','*')
+res.header('Access-Control-Allow-Origin','http://localhost:3000')
 // 允许客户端使用哪些请求方法访问我
 res.header('Access-Control-Allow-Methods','get,post')
-        res.send('ok1')
+res.header('Access-Control-Allow-Credentials',true)
+        // res.send('ok1')
     next()
 })
 
@@ -35,6 +43,32 @@ app.get('/cross',(req,res) => {
     // 允许客户端使用哪些请求方法访问我
     res.header('Access-Control-Allow-Methods','get,post')
     res.send('ok')
+})
+app.get('/see',(req,res) =>{
+    res.send('obj')
+})
+
+app.post('/login',(req,res) => {
+    var form = new formidable.IncomingForm()
+    form.parse(req,(err,fields,files) => {
+        // res.send(fields)
+        const {username, password} = fields
+        if (username == 'zs'&& password == '123') {
+        // 设置ression
+        req.session.isLogin = true
+            res.send({message: '登录成功'})
+        }else {
+            res.send({message: '登录失败，用户名或者密码错误'})
+        }
+    })
+})
+app.get('/checkLogin',(req,res) => {
+    
+    if (req.session.isLogin) {
+        res.send({message:'已经登录'})
+    }else {
+        res.send({message:'还未登录'})
+    }
 })
 app.listen(3001)
 console.log('s2服务器启动成功'); 
