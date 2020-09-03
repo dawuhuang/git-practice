6e73f68be56589a5d44419b179818ab63404f1dd
dawuhuang <jk996zz@163.com>
2020-09-03 21:06:31 +0800

    第42天elementui使用

--- day42vue/2-webpack/vue_proj_01/src/main.js
+++ day42vue/2-webpack/vue_proj_01/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 导入组件库
+import ElementUI from 'element-ui'
+// 导入组件相关样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 配置Vue插件
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
