65fcbb701e721720e41867c3720d4c5bcc8402b8
yang <986996496@qq.com>
2020-09-20 11:47:59 +0800

    增加路由导航守卫

--- day51/assets/js/index.js
+++ day51/assets/js/index.js
@@ -1,5 +1,23 @@
 $(function() {
     var layer = layui.layer
+    // console.log(window.location.pathname);
+    if(window.location.pathname == '/' || window.location.pathname == '/index.html') {
+        $.ajax({
+            method: 'get',
+            url: '/my/userinfo',
+            headers: {
+                'Authorization': window.localStorage.getItem("token") || '',
+            },
+            success: function(res) {
+                console.log(res);
+                if (res.status == 1) {
+                    window.localStorage.removeItem('token')
+                    location.href = '/login.html'
+                }
+            }
+        })
+    }
+
     $('#logOut').on('click', function() {
         layer.confirm('确认退出？', {icon: 3, title:'提示'}, function(index){
             // 跳转页面
