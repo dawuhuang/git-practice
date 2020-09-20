a80004331bccdeae34839938de7e9c1f3c624b19
yang <986996496@qq.com>
2020-09-20 15:56:25 +0800

    完成基本资料页面

--- day51/assets/js/index.js
+++ day51/assets/js/index.js
@@ -1,11 +1,25 @@
 $(function () {
     var layer = layui.layer
     // console.log(window.location.pathname);
+    
+    getUserInfo()
+    $('#logOut').on('click', function () {
+        layer.confirm('确认退出？', { icon: 3, title: '提示' }, function (index) {
+            // 跳转页面
+            location.href = '/login.html'
+            // 请除token
+            window.localStorage.removeItem('token')
+            layer.close(index);
+        });
+    })
+
+
+})
+function getUserInfo() {
     if (window.location.pathname == '/' || window.location.pathname == '/index.html') {
         $.ajax({
             method: 'get',
             url: '/my/userinfo',
-
             success: function (res) {
                 // console.log(res);
                 if (res.status == 1) {
@@ -17,45 +31,35 @@ $(function () {
             }
         })
     }
-
-    $('#logOut').on('click', function () {
-        layer.confirm('确认退出？', { icon: 3, title: '提示' }, function (index) {
-            // 跳转页面
-            location.href = '/login.html'
-            // 请除token
-            window.localStorage.removeItem('token')
-            layer.close(index);
-        });
-    })
-    function userInfo(data) {
-        // 第一优先级 图片 第二优先级 昵称 第三优先级用户名
-        if (data.user_pic) {
-            // 字符头像隐藏
-            $('.text-avatar').hide()
-            // 图片头像显示
-            $('.layui-nav-img').show().prop('src', data.user_pic)
-            $('.text-avatar').siblings('span').text('欢迎：' + data.username)}
-        //  else if (data.nickname) {
-        //     // 图片头像隐藏
-        //     $('.layui-nav-img').hide()
-        //     // 字符头像显示
-        //     // 找出头像的字符串第一个
-        //     $('.text-avatar').show().html(data.nickname[0].toUpperCase())
-        //     $('.text-avatar').siblings('span').text('欢迎：' + data.username)
-        // } else {
-        //     // 图片头像隐藏
-        //     $('.layui-nav-img').hide()
-        //     // 字符头像显示
-        //     // 找出头像的字符串第一个
-        //     $('.text-avatar').show().html(data.username[0].toUpperCase())
-        //     $('.text-avatar').siblings('span').text('欢迎：' + data.username)
-        // }
-        // 代码优化
-        else {
-            $('.layui-nav-img').hide()
-            $('.text-avatar').siblings('span').text('欢迎：' + data.username)
-            var name = data.nickname || data.username || '**'
-            $('.text-avatar').show().html(name[0].toUpperCase())
-        }
+}
+function userInfo(data) {
+    // 第一优先级 图片 第二优先级 昵称 第三优先级用户名
+    var name = data.nickname || data.username || '**'
+    if (data.user_pic) {
+        // 字符头像隐藏
+        $('.text-avatar').hide()
+        // 图片头像显示
+        $('.layui-nav-img').show().prop('src', data.user_pic)
+        $('.text-avatar').siblings('span').text('欢迎：' + name)}
+    //  else if (data.nickname) {
+    //     // 图片头像隐藏
+    //     $('.layui-nav-img').hide()
+    //     // 字符头像显示
+    //     // 找出头像的字符串第一个
+    //     $('.text-avatar').show().html(data.nickname[0].toUpperCase())
+    //     $('.text-avatar').siblings('span').text('欢迎：' + data.username)
+    // } else {
+    //     // 图片头像隐藏
+    //     $('.layui-nav-img').hide()
+    //     // 字符头像显示
+    //     // 找出头像的字符串第一个
+    //     $('.text-avatar').show().html(data.username[0].toUpperCase())
+    //     $('.text-avatar').siblings('span').text('欢迎：' + data.username)
+    // }
+    // 代码优化
+    else {
+        $('.layui-nav-img').hide()
+        $('.text-avatar').siblings('span').text('欢迎：' + name)
+        $('.text-avatar').show().html(name[0].toUpperCase())
     }
-})
+}
---
+++ day51/assets/js/user/user_info.js
@@ -0,0 +1,52 @@
+$(function() {
+    var layer = layui.layer
+    var form = layui.form
+    userInfo()
+    function userInfo() {
+        $.ajax({
+            method: 'get',
+            url: '/my/userinfo',
+            success: function(res) {
+                // console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('获取用户基本信息失败')
+                }
+                form.val('userInfoForm',res.data)
+            }
+        })
+    }
+    $('#reset').on('click',function(e) {
+        e.preventDefault()
+        userInfo()
+    })
+    // 对昵称进行验证
+    form.verify({
+        nickname: function (value) { //value：表单的值、item：表单的DOM对象
+            if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+                return '昵称名不能有特殊字符';
+            }
+            if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+                return '昵称名首尾不能出现下划线\'_\'';
+            }
+        }
+    })
+    // 对用户信息进行更正
+    $('#modifyUserInfo').on('submit',function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'post',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function(res) {
+                // console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg("修改用户信息失败")
+                }
+                layer.msg('修改用户信息成功！', {icon: 6}); 
+                // 跳转到首页
+                window.parent.getUserInfo()
+            }
+        })
+    })
+
+})
